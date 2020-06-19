d5180bb99a5df044ccd8254a1d6235dad236c7c8
NicoRondan <nicolasrondan@live.com.ar>
2020-06-19 08:47:10 -0300

    termino app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, Response, flash, session
-from db import existing_user, add_user, initialize, get_galactic_coins
+from db import existing_user, add_user, initialize, get_galactic_coins, get_users, get_weapons, transfer_coins, get_user_by_name, transfer_weapon, add_weapon
 import bcrypt
 
 
@@ -16,12 +16,79 @@ def initialize_db():
 
 @app.route('/')
 def index():
+    #En caso de que el usuario no haya iniciado sesion
     if 'public_key' not in session or session['public_key'] == None:
         return redirect(url_for('login'))
     else:
-        res = get_galactic_coins(session['public_key'])
-        #Renderizar index
-        return render_template('index.html', coins=res['galactic_coins'])
+        #Obtener monedas del user
+        coins = get_galactic_coins(session['public_key'])
+        if session['username'] == 'Darth Vader':
+            #Users para transferir coins
+            users = get_users()
+            #Armas para la venta
+            weapons = get_weapons(session['public_key'])
+            return render_template('index.html', users=users, coins=coins, weapons=weapons, isAdmin=True)
+        else:
+            darth_vader = get_user_by_name('Darth Vader')
+            weapons = get_weapons(darth_vader['public_key'])
+            #Renderizar index
+            return render_template('index.html', coins=coins, weapons=weapons, isAdmin=False)
+
+@app.route('/transfer-coins', methods=['POST'])
+def transfer_galactic_coins():
+    username = request.form.get('users')
+    coins = request.form.get('amount')
+    user_transfer = {
+        'public_key': session['public_key'],
+        'private_key': session['private_key']
+    }
+    transfer_coins(user_transfer, username, coins)
+    return redirect(url_for('index'))
+
+@app.route('/buy-weapon/<id>/<coins>/<price>', methods=['GET'])
+def buy_weapon(id, coins, price):
+    if (int(coins) < int(price)):
+        flash('Insufficient Galactic Coins!')
+        return redirect(url_for('index'))
+    else:
+        user_transfer = {
+        'public_key': session['public_key'],
+        'private_key': session['private_key']
+        }
+        #Pagamos
+        transfer_coins(user_transfer, 'Darth Vader', price)
+        #Transferimos arma
+        transfer_weapon(user_transfer, id)
+        flash('Weapon Adquired!')
+        return redirect(url_for('user_weapons'))
+
+@app.route('/add-weapon', methods=['GET', 'POST'])
+def new_weapon():
+    if request.method == 'POST':
+        #Obtener datos
+        name = request.form.get('name')
+        price = request.form.get('price')
+        amount = request.form.get('amount')
+        darth_vader = {
+            'public_key': session['public_key'],
+            'private_key': session['private_key']
+        }
+        try:
+            add_weapon(darth_vader, name, price, amount)
+            flash('Weapon Added Successfully!')
+            return redirect(url_for('index'))
+        except Exception as err:
+            print("An exception occurred :", err)
+            flash('Error...')
+    return render_template('add-weapon.html')
+
+
+@app.route('/user-weapons', methods=['GET'])
+def user_weapons():
+    #Armas del user
+    weapons = get_weapons(session['public_key'])
+    #renderizar armas de user
+    return render_template('user-weapons.html', weapons=weapons)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
--- db.py
+++ db.py
@@ -32,6 +32,13 @@ def get_user_by_name(name):
     user = users.find_one({'username': name}, {'_id': 0, 'public_key': 1, 'private_key': 2})
     return user
 
+def get_users():
+    db = connect_db()
+    users = db.users
+    #Obtener todos menos a darth vader
+    result = users.find({'username': {'$ne': 'Darth Vader' }}, {'_id': 0, 'password': 0})
+    return result
+
 def add_user(user):
     db = connect_db()
     #Generar claves
@@ -44,38 +51,222 @@ def add_user(user):
     #Retornar claves para la session
     return user['public_key'], user['private_key']
 
+def get_assets(public_key, criterion):
+    #Obtener los activos
+    transactions = []
+    #Obtener outputs sin gastar del respectivo user
+    outputs = bdb.outputs.get(public_key, False)
+    for item in outputs:
+        #Recuperar la transacción en base al id de la transaccion del output
+        item["transaction"] = bdb.transactions.retrieve(txid=item["transaction_id"])
+        #Si no hay metadata quiere decir que es una transaccion de moneda
+        if item["transaction"]["metadata"] == None and criterion == 'coins' :
+            transactions.append(item)
+        elif item["transaction"]["metadata"] != None and criterion == 'weapons':
+            transactions.append(item)
+    return transactions
+
 def get_galactic_coins(public_key):
-    #Obtener los assets
-    asset = bdb.assets.get(search='Galactic Coins')[0]
-    transaction = bdb.transactions.get(asset_id=asset['id'])
-    galactic_coins = None
-    transaction_id = None
-    output_index = None
-    output = None
-    index = 0
-    for item in transaction[0]['outputs']:
-        #Recorremos los outputs
-        if public_key in item['public_keys']:
-            output_index = index
-            galactic_coins = item['amount']
-            break
-        if galactic_coins != None:
-            transaction_id = item["id"]
-            output = item["outputs"][output_index]
-            break
-        i =+ 1    
-    obj = {
-        'galactic_coins': galactic_coins,
-        'transaction_id': transaction_id,
-        'output_index': output_index,
-        'output': output,
+    transactions_coins = get_assets(public_key, "coins")
+    value = 0
+    for element in transactions_coins:
+        outputs = element["transaction"]["outputs"]
+        #Posicion de la transaccion del user
+        output = outputs[element["output_index"]]
+        #Sumar el valor del amount por cada transaccion
+        value += int(output["amount"])
+    return value
+
+def get_weapons(public_key):
+    transactions_weapons = get_assets(public_key, 'weapons')
+    #print(transactions_weapons)
+    weapons = []
+    for item in transactions_weapons:
+        if item["transaction"]["operation"] != 'CREATE':
+            id = item["transaction"]["asset"]["id"]
+            data = bdb.transactions.get(asset_id=id, operation='CREATE')
+            #print(data)
+            data = data[0]["asset"]
+            asset_id = item["transaction"]["asset"]["id"]
+        else:
+            data = item["transaction"]["asset"]
+            asset_id = item["transaction"]["id"]
+        #Obtener cantidad de armas
+        outputs = item["transaction"]["outputs"]
+        output =  outputs[item["output_index"]]
+        amount = int(output["amount"])
+        #Generar arma 
+        weapon = {
+            "id":asset_id,
+            "data":data["data"],
+            "amount":amount
+        }
+        #En caso de que el arma ya esté en la lista, se suman las cantidades
+        if weapon not in weapons:
+            weapons.append(weapon)
+        else:
+            for element in weapons:
+                if (element["id"] == weapon["id"]):
+                    element["amount"] = element["amount"] + amount
+                    break
+    return weapons
+
+def transfer_coins(user1,user2,coins):
+    user2 = get_user_by_name(user2)
+    transactions_coins = get_assets(user1['public_key'],'coins')
+    lost_coins = int(coins)
+    i = 0
+    while lost_coins != 0 and i < len(transactions_coins):
+        item = transactions_coins[i]
+        outputs = item["transaction"]["outputs"]
+        output = outputs[item["output_index"]]
+        amount = int(output["amount"])
+
+        '''Iniciar transferencia'''
+        transfer_input = {
+                'fulfillment': output['condition']['details'],
+                'fulfills': {
+                'output_index': item["output_index"],
+                'transaction_id': item["transaction_id"],
+            },
+                'owners_before': output['public_keys'],
+            }
+        if (item["transaction"]["operation"] == 'CREATE'):
+            transfer_asset = {
+            'id': item["transaction"]["id"],
+            }
+        else:
+            transfer_asset = {
+            'id': item["transaction"]["asset"]["id"],
+            }    
+
+        if (amount > lost_coins):
+            '''Preparar transferencia'''
+            prepared_transfer_tx = bdb.transactions.prepare(
+            operation='TRANSFER',
+            asset=transfer_asset,
+            inputs=transfer_input,
+            recipients=[([user2['public_key']], lost_coins), ([user1['public_key']], amount - lost_coins )]
+            )
+            #Firmar
+            fulfilled_transfer_tx = bdb.transactions.fulfill(prepared_transfer_tx, private_keys=user1['private_key'])
+            #Enviar nodo a bigchaindb
+            bdb.transactions.send_commit(fulfilled_transfer_tx)
+
+            lost_coins = 0
+        #En caso de que se entreguen todos los coins disponibles
+        elif (amount == lost_coins):
+            prepared_transfer_tx = bdb.transactions.prepare(
+            operation='TRANSFER',
+            asset=transfer_asset,
+            inputs=transfer_input,
+            recipients=[([user2['public_key']], lost_coins)]
+            )
+
+            fulfilled_transfer_tx = bdb.transactions.fulfill(prepared_transfer_tx, private_keys=user1['private_key'])
+
+            bdb.transactions.send_commit(fulfilled_transfer_tx)
+
+            lost_coins = 0
+        else:
+            prepared_transfer_tx = bdb.transactions.prepare(
+            operation='TRANSFER',
+            asset=transfer_asset,
+            inputs=transfer_input,
+            recipients=[([user2['public_key']], amount)]
+            )
+
+            fulfilled_transfer_tx = bdb.transactions.fulfill(prepared_transfer_tx, private_keys=user1['private_key'])
+
+            bdb.transactions.send_commit(fulfilled_transfer_tx)
+
+            lost_coins = lost_coins - amount
+        i += 1
+
+def transfer_weapon (user, id_weapon):
+    darth_vader = get_user_by_name('Darth Vader')
+    transactions_weapons = get_assets(darth_vader['public_key'], 'weapons')
+    metadata = {"type":"weapon"}
+    
+    for item in transactions_weapons:
+        
+        operation = item["transaction"]["operation"]
+        
+        if (operation == 'CREATE' and  item["transaction"]["id"] == id_weapon) or (operation == 'TRANSFER' and item["transaction"]["asset"]["id"] == id_weapon):
+            outputs = item["transaction"]["outputs"]
+            output = outputs[item["output_index"]]
+            amount = int(output["amount"])
+
+            '''iniciar transferencia'''
+            transfer_input = {
+                    'fulfillment': output['condition']['details'],
+                    'fulfills': {
+                    'output_index': item["output_index"],
+                    'transaction_id': item["transaction_id"],
+                },
+                    'owners_before': output['public_keys'],
+                }
+
+            if (operation == 'CREATE'):
+                transfer_asset = {
+                'id': item["transaction"]["id"],
+                }
+            else:
+                transfer_asset = {
+                'id': item["transaction"]["asset"]["id"],
+                }    
+
+            if (amount == 1):
+                prepared_transfer_tx = bdb.transactions.prepare(
+                operation='TRANSFER',
+                asset=transfer_asset,
+                inputs=transfer_input,
+                metadata=metadata,
+                recipients=[([user['public_key']], amount)]
+                )
+
+                fulfilled_transfer_tx = bdb.transactions.fulfill(prepared_transfer_tx, private_keys=darth_vader['private_key'])
+                bdb.transactions.send_commit(fulfilled_transfer_tx)
+            else:
+                prepared_transfer_tx = bdb.transactions.prepare(
+                operation='TRANSFER',
+                asset=transfer_asset,
+                inputs=transfer_input,
+                metadata=metadata,
+                recipients=[([user['public_key']], 1),([darth_vader['public_key']], amount - 1)]
+                )
+
+                fulfilled_transfer_tx = bdb.transactions.fulfill(prepared_transfer_tx, private_keys=darth_vader['private_key'])
+                bdb.transactions.send_commit(fulfilled_transfer_tx)
+
+def add_weapon(darth_vader, name, price, amount):
+    weapon = {
+        "data": {
+            "name": name,
+            "price": price
+        }
     }
-    return obj
+    metadata = {'type': 'weapon'}
+    #Preparamos la transacción
+    prepared_creation_tx = bdb.transactions.prepare(
+    operation='CREATE',
+    signers=darth_vader['public_key'],
+    recipients=[([darth_vader['public_key']], int(amount))],
+    asset=weapon,
+    metadata=metadata
+    )
+    #Completamos la transacción
+    fulfilled_creation_tx = bdb.transactions.fulfill(
+    prepared_creation_tx,
+    private_keys=darth_vader['private_key']
+    )
+    # Y enviamos el nodo a bigchaindb
+    bdb.transactions.send_commit(fulfilled_creation_tx)
 
 def initialize():
     db = connect_db()
     users = db.users
-    #users.drop()#
+    #users.drop()
     if users.count() == 0:
         try:
             #Insertar archivos json contenidos en users.json
@@ -102,7 +293,6 @@ def initialize():
                     'name': "Galactic Coins",
                 },
         }
-#
 
         #Obtenemos usuarios con sus respectivas keys
         darth_vader = get_user_by_name('Darth Vader')
@@ -136,7 +326,7 @@ def initialize():
             prepared_creation_tx = bdb.transactions.prepare(
             operation='CREATE',
             signers=darth_vader['public_key'],
-            recipients=[([darth_vader['public_key']], 10)],
+            recipients=[([darth_vader['public_key']], 7)],
             asset=weapon,
             metadata=metadata
             )
@@ -147,44 +337,3 @@ def initialize():
             )
             # Y enviamos el nodo a bigchaindb
             bdb.transactions.send_commit(fulfilled_creation_tx)
-
-
-
-
-
-
-
-
-
-
-'''Iniciar transferencia'''
-        # transfer_asset = {
-        #     'id': fulfilled_galactic_coins_tx['id']
-        # }
-        # output_index = 0
-        # output = fulfilled_galactic_coins_tx['outputs'][output_index]
-        # transfer_input = {
-        #     'fulfillment': output['condition']['details'],
-        #     'fulfills': {
-        #         'output_index': output_index,
-        #         'transaction_id': transfer_asset['id']
-        #     },
-        #     'owners_before': output['public_keys']
-        # }
-
-        # #Preparamos la transferencia
-        # prepared_transfer_tx = bdb.transactions.prepare(
-        # operation='TRANSFER',
-        # asset=transfer_asset,
-        # inputs=transfer_input,
-        # recipients=[([bobba_fett['public_key']], 5), ([greedo['public_key']], 3), ([din_djarin['public_key']], 8), ([darth_vader['public_key']], 4)]
-        # )
-        # #
-        # #Completamos la transferencia
-        # fulfilled_transfer_tx = bdb.transactions.fulfill(
-        #     prepared_transfer_tx,
-        #     private_keys=darth_vader['private_key'],
-        # )
-
-        # #Commiteamos
-        # sent_transfer_tx = bdb.transactions.send_commit(fulfilled_transfer_tx)
